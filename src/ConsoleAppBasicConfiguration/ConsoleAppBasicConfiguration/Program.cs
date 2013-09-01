5a14ec227223b219bca5671986773911b70654a5
Steven D'Anna <steven.danna@gmail.com>
2013-09-01 18:51:09 -0500

    Initializing Logging

--- src/ConsoleAppBasicConfiguration/ConsoleAppBasicConfiguration/Program.cs
+++ src/ConsoleAppBasicConfiguration/ConsoleAppBasicConfiguration/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace ConsoleAppBasicConfiguration
 {
@@ -10,9 +13,15 @@ namespace ConsoleAppBasicConfiguration
     {
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            LogManager.GetLogger(typeof(Program)).Info("===== Application Starting =====");
+
+
+
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+            LogManager.GetLogger(typeof(Program)).Info("===== Application Ending =====");
         }
     }
 }
