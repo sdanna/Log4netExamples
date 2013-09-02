daebac5665ccb1056379cdb7340789081ee333f1
Steven D'Anna <steven.danna@gmail.com>
2013-09-01 20:04:30 -0500

    Refactored Console App to use the new logmanager and added in AppDomain unhandled exception event handler with logging

--- src/ConsoleAppBasicConfiguration/ConsoleAppBasicConfiguration/Program.cs
+++ src/ConsoleAppBasicConfiguration/ConsoleAppBasicConfiguration/Program.cs
@@ -4,24 +4,48 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleAppBasicConfiguration.Logging;
 using log4net;
 using log4net.Config;
 
 namespace ConsoleAppBasicConfiguration
 {
-    class Program
+    public class Program
     {
+        private static Logging.LogManager _logManager = new Logging.LogManager();
+
         static void Main(string[] args)
         {
             XmlConfigurator.Configure(new FileInfo("log4net.config"));
-            LogManager.GetLogger(typeof(Program)).Info("===== Application Starting =====");
+            var logger = _logManager.GetLogger<Program>();
+            logger.Info("===== Application Starting =====");
 
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
 
+            logger.Debug("Debug Message");
+            logger.Info("Info Message");
+            logger.Warn("Warning Message");
+            logger.Error("Error Message");
+            logger.Fatal("Error Message");
 
-
+            logger.Info("===== Application Ending =====");
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-            LogManager.GetLogger(typeof(Program)).Info("===== Application Ending =====");
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            var exception = unhandledExceptionEventArgs.ExceptionObject as Exception;
+            if (exception != null)
+            {
+                var logger = _logManager.GetLogger<Program>();
+                logger.Error("An unhandled exception has occured.", exception);
+                logger.Info("===== Application Ending =====");
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
         }
     }
 }
