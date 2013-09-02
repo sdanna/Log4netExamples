afa8aac2d1cf40f72a6b4628634da8b6e44a08b5
Steven D'Anna <steven.danna@gmail.com>
2013-09-01 20:03:18 -0500

    Adding a better instance based LogManager

---
+++ src/ConsoleAppBasicConfiguration/ConsoleAppBasicConfiguration/Logging/LogManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ConsoleAppBasicConfiguration.Logging
+{
+    public class LogManager
+    {
+        /// <summary>
+        /// Gets a log4net Logger for the specified generic type.
+        /// </summary>
+        /// <typeparam name="T">Named logger type to get.</typeparam>
+        /// <returns>ILog singleton instance for the specified generic type.</returns>
+        public ILog GetLogger<T>()
+        {
+            var logger = log4net.LogManager.GetLogger(typeof (T));
+            return logger;
+        }
+    }
+}
